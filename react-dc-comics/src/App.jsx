aee5d2153e988711d27dbb342a0439abfb18d6dc
munteanudenis87 <munteanudenis87@gmail.com>
2026-02-06 23:49:24 +0100

    first commit

---
+++ react-dc-comics/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./assets/components/Header.jsx";
+import Main from "./assets/components/Main.jsx";
+import Footer from "./assets/components/Footer.jsx";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
---
+++ react-dc-comics/src/assets/components/Header.jsx
@@ -0,0 +1,39 @@
+export default function Header() {
+    return (<header>
+        <figure>
+            <img src="" alt="" />
+        </figure>
+        <nav>
+            <ul>
+                <li><a href=""></a>CHARACTERS</li>
+            </ul>
+            <ul>
+                <li><a href=""></a>COMICS</li>
+            </ul>
+            <ul>
+                <li><a href=""></a>MOVIES</li>
+            </ul>
+            <ul>
+                <li><a href=""></a>TV</li>
+            </ul>
+            <ul>
+                <li><a href=""></a>GAMES</li>
+            </ul>
+            <ul>
+                <li><a href=""></a>COLLECTIBLES</li>
+            </ul>
+            <ul>
+                <li><a href=""></a>VIDEOS</li>
+            </ul>
+            <ul>
+                <li><a href=""></a>FANS</li>
+            </ul>
+            <ul>
+                <li><a href=""></a>NEWS</li>
+            </ul>
+            <ul>
+                <li><a href=""></a>SHOP</li>
+            </ul>
+        </nav>
+    </header>);
+}
